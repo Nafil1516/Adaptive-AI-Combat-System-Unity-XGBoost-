65c73946f0f26398da2e386dcc730763fab2e802
MUHAMMADWASEEM22095 <130099908+MUHAMMADWASEEM22095@users.noreply.github.com>
2025-10-31 03:04:27 +0500

    movement done

---
+++ Assets/SCRIPTS/Controller.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Animator bananaAnimator;
+    AnimatorStateInfo animationstate;
+    
+    void Start()
+    {
+        bananaAnimator = this.GetComponent<Animator>();
+        if (bananaAnimator == null)
+        {
+            Debug.LogError("No animator component found on this GameObject.");
+        }
+        else
+        {
+            Debug.Log("Animator component successfully found.");
+        }
+    }
+
+    
+    void FixedUpdate()
+    {
+        animationstate = bananaAnimator.GetCurrentAnimatorStateInfo(0);
+        if(animationstate.IsName("rolling") || animationstate.IsName("slide"))
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += this.transform.forward * 0.01f;
+            bananaAnimator.GetFloat("speed");
+            bananaAnimator.SetFloat("speed", 0.6f);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                bananaAnimator.GetFloat("speed");
+                bananaAnimator.SetFloat("speed", 1.1f);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    bananaAnimator.SetTrigger("Rolling");
+                }
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    bananaAnimator.SetTrigger("slide");
+                }
+
+            }
+        }
+        else
+        {
+            bananaAnimator.GetFloat("speed");
+            bananaAnimator.SetFloat("speed", 0.1f);
+        }
+
+    }
+}
