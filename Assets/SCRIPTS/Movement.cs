6cc2ed0aa63f3b00cb771afc17e462034a1abe92
MUHAMMADWASEEM22095 <130099908+MUHAMMADWASEEM22095@users.noreply.github.com>
2025-11-02 02:49:02 +0500

    working on character controller

--- Assets/SCRIPTS/Movement.cs
+++ Assets/SCRIPTS/Movement.cs
@@ -3,15 +3,46 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
+    [Header("References")]
     public Animator animator;
+    CharacterController CC;
 
+
+    [Header("Movement Speeds")]
     public float walk = 5f;
     public float run = 10f;
     public float roll = 7f;
     public float slide = 9f;
-    
+
+    [Header("Animation States")]
+    public int movespeed = Animator.StringToHash("speed");
+    public int rollstate = Animator.StringToHash("Rolling");
+    public int slidestate = Animator.StringToHash("slide");
+
+    private AnimatorStateInfo animationstate;
+
+
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on " + gameObject.name);
+        }
+        CC = GetComponent<CharacterController>();
+        if (CC == null)
+        {
+            Debug.LogError("CharacterController component not found on " + gameObject.name);
+        }
+    }
+
     void Start()
     {
+        animator.SetFloat(movespeed, 0f);
+        animationstate= animator.GetCurrentAnimatorStateInfo(0);
+        Debug.Log("Movement script initialized.");
+        Debug.Log("Current Animation Played: IDLE: " + animationstate.IsName("happy idle"));
         
     }
 
